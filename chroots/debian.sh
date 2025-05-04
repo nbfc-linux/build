b9bfa5d887a6069157e3445bb6cebfb63cf01499
Benjamin Abendroth <braph93@gmx.de>
2025-05-04 15:07:56 +0200

    Changed debian.sh

--- chroots/debian.sh
+++ chroots/debian.sh
@@ -2,6 +2,7 @@
 
 ARCH=amd64
 CHROOT_DIR=/tmp/debian.nbfc-linux
+CACHE_DIR=/tmp/debian.cache
 RELEASE=bookworm
 
 set -e
@@ -17,5 +18,11 @@ pacman -S --needed debootstrap
 # Make chroot dir
 mkdir -p "$CHROOT_DIR"
 
+# Make cache dir
+mkdir -p "$CACHE_DIR"
+
 # Install debian into chroot
-debootstrap --arch=$ARCH $RELEASE "$CHROOT_DIR" http://deb.debian.org/debian
+debootstrap \
+  --arch=$ARCH \
+  --cache-dir="$CACHE_DIR" \
+  $RELEASE "$CHROOT_DIR" http://deb.debian.org/debian
